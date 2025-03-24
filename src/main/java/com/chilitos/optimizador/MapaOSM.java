2d79340e60a2ee9e2c584d793a9bcfb09eae07b9
Omar <omarnataren@outlook.com>
2025-03-23 19:54:27 -0600

    Mapa inicial JavaFX

--- src/main/java/com/chilitos/optimizador/MapaOSM.java
+++ src/main/java/com/chilitos/optimizador/MapaOSM.java
@@ -1,6 +1,34 @@
+package com.chilitos.optimizador;
+
+import javafx.scene.web.WebView;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
 
 
+public class MapaOSM extends BorderPane {
 
+    public MapaOSM() {
+        WebView webView = new WebView();
+        WebEngine engine = webView.getEngine();
 
+        engine.load(getClass().getResource("/mapa.html").toExternalForm());
+        this.setCenter(webView);
+
+        VBox barraLateral = new VBox(10);
+        barraLateral.setPadding(new Insets(10));
+        barraLateral.setStyle("-fx-background-color:rgb(216, 229, 143);");
+
+        Label opciones = new Label("Opciones: ");
+        Button button1 = new Button("Añadir nodo");
+        Button button2 = new Button("Optimizar");
+
+        barraLateral.getChildren().addAll(opciones, button1, button2);
+        this.setLeft(barraLateral);
+    }
+}
 
 
